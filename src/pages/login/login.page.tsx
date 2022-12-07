cced855f22d7976c547122b61882d261385ac412
Cleiton Sobrinho <cleiton1.life@gmail.com>
2022-12-06 21:45:29 -0300

    feat: add CostomButton to LoginPage

--- src/pages/login/login.page.tsx
+++ src/pages/login/login.page.tsx
@@ -1,6 +1,11 @@
 import { BsGoogle } from 'react-icons/bs'
+import { FiLogIn } from 'react-icons/fi'
+
+// Components
 import Header from '../../components/header/header.component'
 import CustomButton from '../../components/custom-button/custom-button.component'
+
+// Styles
 import { LoginContainer, LoginHeadline, LoginInputContainer, LoginSubtitle, LoginContent } from './login.styles'
 
 const LoginPage = () => {
@@ -19,7 +24,7 @@ const LoginPage = () => {
                 <LoginInputContainer>{/* Email input */}</LoginInputContainer>
                 <LoginInputContainer>{/* password input */}</LoginInputContainer>
 
-                {/* Botton */}
+                <CustomButton startIcon={<FiLogIn size={18} />}>Entrar</CustomButton>
                 </LoginContent>
 
            </LoginContainer>
