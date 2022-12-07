11a814f831ca0de4935c92e1dc2029e7a032ac99
Cleiton Sobrinho <cleiton1.life@gmail.com>
2022-12-06 21:32:35 -0300

    feat: add CustomButton Component

--- src/pages/login/login.page.tsx
+++ src/pages/login/login.page.tsx
@@ -1,4 +1,6 @@
+import { BsGoogle } from 'react-icons/bs'
 import Header from '../../components/header/header.component'
+import CustomButton from '../../components/custom-button/custom-button.component'
 import { LoginContainer, LoginHeadline, LoginInputContainer, LoginSubtitle, LoginContent } from './login.styles'
 
 const LoginPage = () => {
@@ -10,7 +12,7 @@ const LoginPage = () => {
                 <LoginContent>
                 <LoginHeadline>Entre com sua conta </LoginHeadline>
 
-                    {/*  Button */ }
+                <CustomButton startIcon={<BsGoogle size={18} /> } >Entrar com Google</CustomButton>
 
                 <LoginSubtitle> ou entre com seu e-mail</LoginSubtitle>
 
