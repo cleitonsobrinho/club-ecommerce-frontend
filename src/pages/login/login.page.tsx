dd1e456ba7177b1708d08714102cb9559b422d0b
Cleiton Sobrinho <cleiton1.life@gmail.com>
2022-12-09 20:40:50 -0300

    feat: add error messagens to Login Pages and custom email validation

--- src/pages/login/login.page.tsx
+++ src/pages/login/login.page.tsx
@@ -1,14 +1,16 @@
 import { BsGoogle } from 'react-icons/bs'
 import { FiLogIn } from 'react-icons/fi'
+import { useForm } from 'react-hook-form'
+import validator from 'validator'
 
 // Components
 import Header from '../../components/header/header.component'
 import CustomButton from '../../components/custom-button/custom-button.component'
+import { InputErrorMessageContainer } from '../../components/input-error-message/input-error-message'
+import CustomInpunt from '../../components/custom-input/custom-input.component'
 
 // Styles
 import { LoginContainer, LoginHeadline, LoginInputContainer, LoginSubtitle, LoginContent } from './login.styles'
-import CustomInpunt from '../../components/custom-input/custom-input.component'
-import { useForm } from 'react-hook-form'
 
 const LoginPage = () => {
   const {
@@ -21,6 +23,7 @@ const LoginPage = () => {
     console.log({ data })
   }
 
+  console.log(errors)
   return (
         <>
            <Header />
@@ -38,7 +41,20 @@ const LoginPage = () => {
                   <CustomInpunt
                      hasError={!!errors?.email}
                      placeholder='Digite seu e-mail'
-                  {...register('email', { required: true })}/>
+                  {...register('email', {
+                    required: true,
+                    validate: (value) => {
+                      return validator.isEmail(value)
+                    }
+                  })}/>
+
+                  {errors?.email?.type === 'required' && (
+                    <InputErrorMessageContainer>O e-mail é obrigatório.</InputErrorMessageContainer>
+                  )}
+
+                  {errors?.email?.type === 'validate' && (
+                    <InputErrorMessageContainer>Por favor, insira um e-mail válido.</InputErrorMessageContainer>
+                  )}
                 </LoginInputContainer>
 
                 <LoginInputContainer>
@@ -47,6 +63,10 @@ const LoginPage = () => {
                     hasError={!!errors?.password}
                     placeholder='Digite sua senha'
                   {...register('password', { required: true })}/>
+
+                  {errors?.password?.type === 'required' && (
+                    <InputErrorMessageContainer>A senha é obrigatória.</InputErrorMessageContainer>
+                  )}
                 </LoginInputContainer>
 
                 <CustomButton
