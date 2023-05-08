30830c90c78fedd02cf7693d8c1c2c84aee45702
Cleiton Sobrinho <cleiton1.life@gmail.com>
2023-05-08 16:26:42 -0300

    chore: add firebase configuration

---
+++ src/config/firebase.config.ts
@@ -0,0 +1,16 @@
+
+import { initializeApp } from 'firebase/app'
+import { getFirestore } from 'firebase/firestore'
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyB1SIQKAt8X56GlhXZ-wrwdURDHVunZlaI',
+  authDomain: 'club-ecommerce-a93fc.firebaseapp.com',
+  projectId: 'club-ecommerce-a93fc',
+  storageBucket: 'club-ecommerce-a93fc.appspot.com',
+  messagingSenderId: '48829518400',
+  appId: '1:48829518400:web:4fd5f273f44dd84be0c5a5'
+}
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig)
+export const db = getFirestore(app)
